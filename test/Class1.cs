285251825d91f4070b10d58bab4c2724805cd58e
Bazukova <67784048+Bazukova@users.noreply.github.com>
2021-10-06 18:51:17 +0300

    Update Class1.cs

--- test/Class1.cs
+++ test/Class1.cs
@@ -13,13 +13,13 @@ namespace ClassLibrary2Z
         {
             if (password.Length < 8 || password.Length > 20)
                 return false;
-            if (!password.Any(Char.IsDigit))
+            if (!password.Any(Char.IsDigit)) // IsDigit показывает, относится ли символ Юникода к категории десятичных цифр.
                 return false;
-            if (!password.Any(Char.IsUpper))
+            if (!password.Any(Char.IsUpper)) // ! - не равно. IsUpper показывает, относится ли символ Юникода к категории букв верхнего регистра.
                 return false;
-            if (!password.Any(Char.IsLower))
-                return false;
-            if (password.Intersect("!#$%^&_)").Count() == 0)
+            if (!password.Any(Char.IsLower)) // Any определяет, соответствует ли коллекция определенному условию, и в зависимости от результата они возвращают true или false. IsLower показывает, относится ли символ Юникода к категории букв нижнего регистра.
+                return false; 
+            if (password.Intersect("!#$%^&_)").Count() == 0) // Intersect получение пересечения последовательностей, то есть общих для обоих наборов элементов
                 return false;
             return true;
         }
